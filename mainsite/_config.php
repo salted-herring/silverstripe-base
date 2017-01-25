cdd18568c7dd5a1e11bdb5d80794ca51fe891022
Simon Winter <simon@saltedherring.com>
2017-01-25 17:56:29 +1300

    phpunit update

--- mainsite/_config.php
+++ mainsite/_config.php
@@ -9,35 +9,55 @@ $database = SS_DATABASE_NAME;
 // Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
 
+/*
+|--------------------------------------------------------------------------
+| For testing, we use sqlite as it's much faster than mysql.
+|--------------------------------------------------------------------------
+*/
+if (Director::isDev()) {
+    if (isset($_GET['db']) && ($db = $_GET['db'])) {
+        global $databaseConfig;
+        if ($db == 'sqlite3') {
+            $databaseConfig['type'] = 'SQLite3Database';
+            $databaseConfig['path'] = ':memory:';
+        }
+    }
+}
+
+/*
+|--------------------------------------------------------------------------
+| Load imagick if available, else fallback to gd.
+|--------------------------------------------------------------------------
+*/
 if (extension_loaded('imagick')) {
-	ImagickBackend::set_default_quality(90);
-	Image::set_backend("ImagickBackend");
+    ImagickBackend::set_default_quality(90);
+    Image::set_backend("ImagickBackend");
 } else {
-	GD::set_default_quality(90);
-	Image::set_backend("OptimisedGDBackend");
+    GD::set_default_quality(90);
+    Image::set_backend("OptimisedGDBackend");
 }
 
 
 Requirements::set_write_js_to_body(false);
 
 if (Director::isLive()) {
-	SS_Log::add_writer(new SS_LogEmailWriter('administration@saltedherring.com'), SS_Log::ERR);
+    SS_Log::add_writer(new SS_LogEmailWriter('administration@saltedherring.com'), SS_Log::ERR);
 }
 
-if(class_exists('Memcache')){
-	SS_Cache::add_backend(
-		'primary_memcached',
-		'Memcached',
-		array(
-			'host' => 'localhost',
-			'port' => 11211,
-			'persistent' => true,
-			'weight' => 1,
-			'timeout' => 1,
-			'retry_interval' => 15,
-			'status' => true,
-			'failure_callback' => ''
-		)
-	);
-	SS_Cache::pick_backend('primary_memcached', 'any', 10);
+if (class_exists('Memcache')) {
+    SS_Cache::add_backend(
+        'primary_memcached',
+        'Memcached',
+        array(
+            'host' => 'localhost',
+            'port' => 11211,
+            'persistent' => true,
+            'weight' => 1,
+            'timeout' => 1,
+            'retry_interval' => 15,
+            'status' => true,
+            'failure_callback' => ''
+        )
+    );
+    SS_Cache::pick_backend('primary_memcached', 'any', 10);
 }
